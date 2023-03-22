f48e8d0b111994893d9c09ead783f32d032f773e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-03-22 15:03:23 +0600

    navbar

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="font-sans antialiased bg-gray-900 text-gray-200">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
