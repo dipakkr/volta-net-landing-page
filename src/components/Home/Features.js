eb038f339b7f3e3c8f742617c922d66ad44047d3
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-03-23 14:46:12 +0600

    morder tool and feature section

--- src/components/Home/Features.js
+++ src/components/Home/Features.js
@@ -1,8 +1,165 @@
+import Image from "next/image";
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <div class="mx-auto max-w-7xl px-4 pb-16 sm:px-6 lg:px-8 ">
+      <div class=" flex flex-col gap-8 lg:grid lg:auto-rows-[3rem] lg:grid-cols-12">
+        <div class="relative col-span-8 row-span-3 flex cursor-pointer flex-col items-center overflow-hidden rounded-lg border-gray-700/50 py-4 transition-colors hover:border-white/10 hover:bg-white/5 lg:border lg:bg-gray-800/50 lg:p-8">
+          <div class="w-full text-center lg:text-left">
+            <div class=" mx-auto mb-4 flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-gray-900/90 lg:mx-0 lg:bg-gray-800/75 lg:group-hover:bg-white/5">
+              <Image
+                src="/assets/home/filter.svg"
+                alt="filter icon"
+                className=" h-5 w-5 flex-shrink-0"
+                width={80}
+                height={80}
+              />
+            </div>
+            <h3 class="text-lg font-medium tracking-tight">Advanced filters</h3>
+            <p class="mt-1">
+              Filter notifications and issues by any property with logical
+              operators.
+            </p>
+            <p class="mt-3 text-sm lg:hidden"> Read more → </p>
+          </div>
+          <div class="hidden lg:block">
+            <Image
+              src="/assets/home/filterImg.svg"
+              alt="filter"
+              loading="lazy"
+              className="absolute left-8 right-0 bottom-4"
+              width={700}
+              height={300}
+            />
+          </div>
+        </div>
+        <div class="relative col-span-4 row-span-6 flex cursor-pointer flex-col items-center overflow-hidden rounded-lg border-gray-700/50 py-4 transition-colors hover:border-white/10 hover:bg-white/5 lg:border lg:bg-gray-800/50 lg:p-8">
+          <div class="w-full text-center lg:text-left">
+            <div class=" mx-auto mb-4 flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-gray-900/90 lg:mx-0 lg:bg-gray-800/75 lg:group-hover:bg-white/5">
+              <Image
+                src="/assets/home/editor.svg"
+                alt="icon"
+                className=" h-5 w-5 flex-shrink-0"
+                width={80}
+                height={80}
+              />
+            </div>
+            <h3 class="text-lg font-medium tracking-tight">Markdown editor</h3>
+            <p class="mt-1">
+              Take advantage of our powerful editor to write markdown with ease.
+            </p>
+            <p class="mt-3 text-sm lg:hidden"> Read more → </p>
+          </div>
+          <div class="hidden lg:block">
+            <Image
+              src="/assets/home/editorImg.svg"
+              alt="Markdown editor"
+              loading="lazy"
+              className="absolute left-8 right-0 bottom-6"
+              width={400}
+              height={400}
+            />
+          </div>
+        </div>
+        <div class=" col-span-4 row-span-3 flex cursor-pointer items-center overflow-hidden rounded-lg border-gray-700/50 py-4 transition-colors hover:border-white/10 hover:bg-white/5 lg:border lg:bg-gray-800/50 lg:p-8">
+          <div class="w-full text-center lg:text-left">
+            <div class=" mx-auto mb-4 flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-gray-900/90 lg:mx-0 lg:bg-gray-800/75 lg:group-hover:bg-white/5">
+              <Image
+                src="/assets/home/mobile.svg"
+                alt="icon"
+                className=" h-5 w-5 flex-shrink-0"
+                width={80}
+                height={80}
+              />
+            </div>
+            <h3 class="text-lg font-medium tracking-tight">Mobile ready</h3>
+            <p class="mt-1">
+              Take advantage of our PWA to use Volta on your mobile or tablet
+              devices.
+            </p>
+            <p class="mt-3 text-sm lg:hidden"> Read more → </p>
+          </div>
+        </div>
+        <div class=" col-span-4 row-span-3 flex items-center overflow-hidden rounded-lg border-gray-700/50 py-4 lg:border lg:bg-gray-800/50 lg:p-8">
+          <div class="w-full text-center lg:text-left">
+            <div class=" mx-auto mb-4 flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-gray-900/90 lg:mx-0 lg:bg-gray-800/75 lg:group-hover:bg-white/5">
+              <Image
+                src="/assets/home/darkLight.svg"
+                alt="icon"
+                className=" h-5 w-5 flex-shrink-0"
+                width={80}
+                height={80}
+              />
+            </div>
+            <h3 class="text-lg font-medium tracking-tight">Light &amp; Dark</h3>
+            <p class="mt-1">
+              Make the Volta app your own and fit your OS. Can be changed
+              anytime.
+            </p>
+          </div>
+        </div>
+        <div class=" col-span-6 row-span-3 flex items-center overflow-hidden rounded-lg border-gray-700/50 py-4 lg:border lg:bg-gray-800/50 lg:p-8">
+          <div class="w-full text-center lg:text-left">
+            <div class=" mx-auto mb-4 flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-gray-900/90 lg:mx-0 lg:bg-gray-800/75 lg:group-hover:bg-white/5">
+              <Image
+                src="/assets/home/keyboard.svg"
+                alt="icon"
+                className=" h-5 w-5 flex-shrink-0"
+                width={80}
+                height={80}
+              />
+            </div>
+            <h3 class="text-lg font-medium tracking-tight">
+              Keyboard-first design
+            </h3>
+            <p class="mt-1">
+              Optimized for efficiency with shortcuts for everything.
+            </p>
+          </div>
+          <div class="hidden lg:block">
+            <Image
+              src="/assets/home/shortcuts.svg"
+              alt="keyboard shortcuts"
+              loading="lazy"
+              className=" mt-[calc(208px-148px)] ml-9"
+              width={400}
+              height={800}
+            />
+          </div>
+        </div>
+        <div class=" col-span-6 row-span-3 flex cursor-pointer items-center overflow-hidden rounded-lg border-gray-700/50 py-4 transition-colors hover:border-white/10 hover:bg-white/5 lg:border lg:bg-gray-800/50 lg:p-8">
+          <div class="w-full text-center lg:text-left">
+            <div class=" mx-auto mb-4 flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-gray-900/90 lg:mx-0 lg:bg-gray-800/75 lg:group-hover:bg-white/5">
+              <Image
+                src="/assets/home/desktop.svg"
+                alt="icon"
+                className=" h-5 w-5 flex-shrink-0"
+                width={80}
+                height={80}
+              />
+            </div>
+            <h3 class="text-lg font-medium tracking-tight">
+              Desktop application
+            </h3>
+            <p class="mt-1">
+              Install Volta on your desktop for a native experience.
+            </p>
+            <p class="mt-3 text-sm lg:hidden"> Read more → </p>
+          </div>
+          <div class="hidden lg:block">
+            <Image
+              src="/assets/home/desktopImg.svg"
+              alt="Desktop application"
+              loading="lazy"
+              className=" mt-[calc(208px-148px)] ml-9"
+              width={400}
+              height={800}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;
--- src/components/Home/ModernTools.js
+++ src/components/Home/ModernTools.js
@@ -1,8 +1,64 @@
+import Image from "next/image";
 
 const ModernTools = () => {
   return (
-    <div>ModernTools</div>
-  )
-}
+    <div>
+      <div class="relative mx-auto max-w-7xl px-4 pb-8 sm:px-6 lg:px-8 ">
+        {/***************** Line Start****************/}
+        <div class="relative mx-auto flex max-w-xs items-center justify-center">
+          <div class="line3 -mr-px h-[1px] w-24 rounded-full"></div>
+          <div class="relative flex items-center justify-center rounded-full">
+            <div class="lineStyle4 absolute -inset-3.5 z-[-1] h-10 w-10 opacity-100 blur-sm will-change-[filter]"></div>
+            <div class="h-3 w-3 rounded-full border-2 border-white"></div>
+          </div>
+        </div>
+        {/***************** Line Start****************/}
+        <h2 class=" mt-8 text-center text-2xl font-bold tracking-tight sm:text-3xl lg:text-4xl">
+          Shipped with modern tools
+        </h2>
+        <p class="u-text-gray-500 mx-auto mt-3 max-w-prose text-center text-lg lg:mt-5">
+          Volta is built for speed to become your day-to-day ally. It is built
+          with a set of features to increase your productivity.
+        </p>
+        {/***************** Tool Content ****************/}
+        <div class="relative mt-16 flex flex-col gap-8 lg:grid lg:auto-rows-[3rem] lg:grid-cols-12">
+          {/***************** Tool Content Main Box ****************/}
+          <div class="tool_gradient absolute inset-0 z-[-1] opacity-10 blur will-change-[filter]"></div>
+          <div class="relative col-span-12 row-span-6 grid cursor-pointer rounded-lg border border-gray-700/50 bg-gray-800/50 transition-colors hover:border-white/10 hover:bg-white/5 lg:grid-cols-2">
+            {/***************** Right ****************/}
+            <div class="aspect-w-16 aspect-h-10 z-[2]  my-6 mx-4 lg:order-last lg:my-auto lg:-ml-8 lg:mr-8">
+              <Image
+                src="/assets/home/inbox.svg"
+                width={1200}
+                height={1200}
+                alt="icon svg"
+                className=" flex-shrink-0"
+              />
+            </div>
+            {/***************** Left ****************/}
+            <div class="relative flex flex-shrink-0 flex-col justify-center p-6 text-center sm:p-16 lg:order-first lg:text-left">
+              <h3 class=" text-xl font-bold tracking-tight sm:text-2xl lg:text-3xl">
+                Powerful command menu
+              </h3>
+              <p class="u-text-gray-500 mt-2 lg:mt-4 lg:text-lg">
+                Search a repository, issue, user or manage anything in seconds
+                from the command menu. <br />
+                <br />
+                Open it with{" "}
+                <kbd class="!my-0 inline-flex h-5 min-w-[20px] items-center justify-center rounded px-1 align-text-top font-sans text-[11px] font-semibold ring-1">
+                  Ctrl
+                </kbd>
+                <span class="inline-block px-1"></span>
+                <kbd class="!my-0 inline-flex h-5 min-w-[20px] items-center justify-center rounded px-1 align-text-top font-sans text-[11px] font-semibold ring-1">
+                  K
+                </kbd>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ModernTools
+export default ModernTools;
